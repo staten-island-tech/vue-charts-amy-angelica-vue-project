9cab2669886c8edb9b5b1bbc906f375a8d560f94
angelicaw133 <angelicawo426@gmail.com>
2024-03-06 10:15:04 -0500

    s

---
+++ vue3-project/src/components/PieChart.vue
@@ -0,0 +1,28 @@
+<template>
+     <div class ="bar">
+    <Bar
+      id="my-chart-id"
+      :options="chartOptions"
+      :data="chartData"
+    />
+  </div>
+  </template>
+  
+  <script lang="ts">
+  import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+  import { Pie } from 'vue-chartjs'
+  import * as chartConfig from ''
+  
+  ChartJS.register()
+  
+  export default {
+    name: 'App',
+    components: {
+      Pie
+    },
+    data() {
+      return chartConfig
+    }
+  }
+  </script>
+  
