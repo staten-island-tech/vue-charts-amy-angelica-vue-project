de7217c17c8faf3b1a8539ff8630c026ffbdd34c
Kelly Ng <kellyng19lover@gmail.com>
2024-03-06 10:14:57 -0500

    th
    tj

--- vue3-project/src/views/AboutView.vue
+++ vue3-project/src/views/AboutView.vue
@@ -1,15 +1,24 @@
 <template>
-  <div class="about">
+  <div class="cotainer">
     <h1>This is an about page</h1>
   </div>
+
+<script setup>
+import {ref,onMounted} from "vue";
+const pokemon =ref ("");
+async function getPokemon(){
+  let res = await fetch("https://data.cityofnewyork.us/d/fak5-wcft");
+  let data = await res.json();
+  pokemon.value= data;
+
+}
+
+onBeforeMount(()=>{
+  getPokemon();
+});
+</script>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
+
 </style>
