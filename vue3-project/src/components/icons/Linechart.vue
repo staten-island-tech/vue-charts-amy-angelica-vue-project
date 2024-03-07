6837f72d756b05ba69e45fd6b314a1427f43863b
Kelly Ng <kellyng19lover@gmail.com>
2024-03-07 10:15:31 -0500

    il
    ul

---
+++ vue3-project/src/components/icons/Linechart.vue
@@ -0,0 +1,50 @@
+<template>
+  <Line
+    id="my-chart-id"
+    :options="chartOptions"
+    :data="chartData"
+  />
+</template>
+
+<script>
+import { Line } from 'vue-chartjs'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+} from 'chart.js'
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+)
+
+export default {
+  name: 'LineChart',
+  components: { Bar },
+  data() {
+    return {
+      chartData: {
+        labels: [ 'January', 'February', 'March' ],
+        data: [65, 59, 80, 81, 56, 55, 40],
+        fill: false,
+        borderColor: 'rgb(75, 192, 192)',
+        tension: 0.1
+  }
+      },
+      chartOptions: {
+        responsive: true
+      }
+    }
+  }
+</script>
