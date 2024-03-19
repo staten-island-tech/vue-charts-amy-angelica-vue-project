04a20faf1bb917ae18d313707c2dddddd49561e8
amyl164 <amyl81008@gmail.com>
2024-03-19 10:13:19 -0400

    sfa

--- vue3-project/src/views/HomeView.vue
+++ vue3-project/src/views/HomeView.vue
@@ -8,41 +8,12 @@
 </template>
 
 <script setup>
- import {array} from '@/stores/store'
 import {ref, onBeforeMount} from "vue";
 const squirrels = ref("")
 async function getSquirrel(){
   let res = await fetch("https://data.cityofnewyork.us/resource/vfnx-vebw.json");
   let data = await res.json();
   squirrels.value = data;
-  let runningsquirrels = data.filter(squirrel => squirrel.running === true)
-  let run = runningsquirrels.length
-  array.cart[0] = run
-
-  let chasingsquirrels = data.filter(squirrel => squirrel.chasing === true)
-  let chase = chasingsquirrels.length
-  array.cart[1] = chase
-
-  let climbingsquirrels = data.filter(squirrel => squirrel.climbing === true)
-  let climb = climbingsquirrels.length
-  array.cart[2] = climb
-
-  let eatingsquirrels = data.filter(squirrel => squirrel.eating === true)
-  let eat = eatingsquirrels.length
-  array.cart[3] = eat
-
-  let foragingsquirrels = data.filter(squirrel => squirrel.foraging === true)
-  let forage = foragingsquirrels.length
-  array.cart[4] = forage
-
-  let kukssquirrels = data.filter(squirrel => squirrel.kuks === true)
-  let kuk = kukssquirrels.length
-  array.cart[5] = kuk
-
-  
-
-  
-  
 }
 onBeforeMount(()=> {
   getSquirrel();
