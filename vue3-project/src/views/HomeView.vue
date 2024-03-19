9889d51bb362ef080f88fbb78cb7e7025e0c9171
amyl164 <amyl81008@gmail.com>
2024-03-18 22:26:44 -0400

    wef

--- vue3-project/src/views/HomeView.vue
+++ vue3-project/src/views/HomeView.vue
@@ -18,21 +18,29 @@ async function getSquirrel(){
   let runningsquirrels = data.filter(squirrel => squirrel.running === true)
   let run = runningsquirrels.length
   array.cart[0] = run
+
   let chasingsquirrels = data.filter(squirrel => squirrel.chasing === true)
   let chase = chasingsquirrels.length
   array.cart[1] = chase
+
   let climbingsquirrels = data.filter(squirrel => squirrel.climbing === true)
   let climb = climbingsquirrels.length
   array.cart[2] = climb
+
   let eatingsquirrels = data.filter(squirrel => squirrel.eating === true)
   let eat = eatingsquirrels.length
   array.cart[3] = eat
+
   let foragingsquirrels = data.filter(squirrel => squirrel.foraging === true)
   let forage = foragingsquirrels.length
   array.cart[4] = forage
+
   let kukssquirrels = data.filter(squirrel => squirrel.kuks === true)
-  let kuk = kukssquirrels
+  let kuk = kukssquirrels.length
   array.cart[5] = kuk
+
+  
+
   
   
 }
