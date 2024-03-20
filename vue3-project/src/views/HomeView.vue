76ac60b40c7ceec6de14c15e820923d5e9ea3199
angelicaw133 <angelicawo426@gmail.com>
2024-03-20 10:12:15 -0400

    lkp,;

--- vue3-project/src/views/HomeView.vue
+++ vue3-project/src/views/HomeView.vue
@@ -8,27 +8,12 @@
 </template>
 
 <script setup>
-
 import {ref, onBeforeMount} from "vue";
 const squirrels = ref("")
 async function getSquirrel(){
   let res = await fetch("https://data.cityofnewyork.us/resource/vfnx-vebw.json");
   let data = await res.json();
   squirrels.value = data;
-
-  let get_grays = data.filter(squirrel=> squirrel.running === "Gray")
-  let gray = get_grays.length
-  array2.cart[0] = gray
-  let get_cinnamons = data.filter(squirrel=> squirrel.running === "Cinnamon")
-  let cinnamon = get_cinnamons.length
-  array2.cart[1] = cinnamon
-  let get_whites = data.filter(squirrel=> squirrel.running === "White")
-  let white = get_whites.length
-  array2.cart[2] = white
-  let get_blacks = data.filter(squirrel=> squirrel.running === "Black")
-  let black = get_blacks.length
-  array2.cart[3] = black
-
 
 }
 onBeforeMount(()=> {
--- vue3-project/src/views/PieView.vue
+++ vue3-project/src/views/PieView.vue
@@ -4,6 +4,32 @@
 
 <script setup>
   import PieChart from '@/components/PieChart.vue'; 
+  import { array2 } from "@/stores/store";
+import {ref, onBeforeMount} from "vue";
+const squirrels = ref("")
+async function getSquirrel(){
+  let res = await fetch("https://data.cityofnewyork.us/resource/vfnx-vebw.json");
+  let data = await res.json();
+  squirrels.value = data;
+
+  let get_grays = data.filter(squirrel=> squirrel.primary_fur_color === "Gray")
+  let gray = get_grays.length;
+  array2.cart[0] = gray
+  let get_cinnamons = data.filter(squirrel=> squirrel.primary_fur_color === "Cinnamon")
+  let cinnamon = get_cinnamons.length
+  array2.cart[1] = cinnamon
+  let get_whites = data.filter(squirrel=> squirrel.primary_fur_color === "White")
+  let white = get_whites.length
+  array2.cart[2] = white
+  let get_blacks = data.filter(squirrel=> squirrel.primary_fur_color === "Black")
+  let black = get_blacks.length
+  array2.cart[3] = black
+
+
+}
+onBeforeMount(()=> {
+  getSquirrel();
+})
 </script>
 
 <style>
